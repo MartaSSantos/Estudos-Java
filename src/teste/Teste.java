6070a7b2aecbf59abbc64b9e1935d084f62fa3a5
Marta S Santos <109400186+MartaSSantos@users.noreply.github.com>
2023-04-10 19:59:35 -0300

    Meu primeiro ola Mudo java

---
+++ src/teste/Teste.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste;
+
+import java.util.Date;
+
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Date relogio = new Date();
+        System.out.println("A Hora doo sistema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
